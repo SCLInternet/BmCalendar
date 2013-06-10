48eda6d8955b6cda7ce1e9b88288dda68d28dd69
Tom Oram <tom@scl.co.uk>
2013-06-10 12:12:03 +0100

    Add license information

--- .travis/travis_build.sh
+++ .travis/travis_build.sh
@@ -17,17 +17,20 @@ LICENSE=0
 
 LICENSE_HEADER="<?php
 /**
-License here
- */"
+  * BmCalendar Module (https://github.com/SCLInternet/BmCalendar)
+  *
+  * @link https://github.com/SCLInternet/BmCalendar for the canonical source repository
+  * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+  */"
 
-#for i in `find src tests -name '*.php'`; do
-#    diff <(echo "$LICENSE_HEADER") <(head -7 "$i");
-#
-#    if [ "$?" -ne "0" ]; then
-#        echo "Missing or invalid license header in \"$i\""
-#        let LICENSE=1
-#    fi
-#done
+for i in `find src tests -name '*.php'`; do
+    diff <(echo "$LICENSE_HEADER") <(head -7 "$i");
+
+    if [ "$?" -ne "0" ]; then
+        echo "Missing or invalid license header in \"$i\""
+        let LICENSE=1
+    fi
+done
 
 ### Display results
 EXIT=0
--- tests/BmCalendarTests/State/MockStateB.php
+++ tests/BmCalendarTests/State/MockStateB.php
@@ -1,4 +1,10 @@
 <?php
+/**
+  * BmCalendar Module (https://github.com/SCLInternet/BmCalendar)
+  *
+  * @link https://github.com/SCLInternet/BmCalendar for the canonical source repository
+  * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+  */
 
 namespace BmCalendarTests\State;
 
